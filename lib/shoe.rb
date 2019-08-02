d9594048cc89671998cf218103b4696c417e9b0c
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-02 15:30:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe
   
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition
   
-  def initialize(brand)
+  def initialize(brand, condition = "new")
     @brand = brand
   end
   
